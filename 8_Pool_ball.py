cdcdb7371be88ea8f3b5a798c3998c1ee2c4e202
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-09-03 14:01:42 -0700

    Update 8_Pool_ball.py

--- 8_Pool_ball.py
+++ 8_Pool_ball.py
@@ -8,24 +8,26 @@ from pygame.locals import*
 pygame.init()
 
 
-#Setting collors
+#Set collors
 white = (255,255,255)
 green = (0,155,0)
 black = (0,0,0)
 
-#Creating an object to help track time so we can define max FPS in our game loop
+#Create an object to help track time so we can define max FPS in our game loop
 clock = pygame.time.Clock()
 
 #Display
+screen_hight = 555
+screen_width = 999
 pygame.display.set_caption("8 Pool Ball")
-screen = pygame.display.set_mode((999,555))
+screen = pygame.display.set_mode((screen_width, screen_hight))
 screen.fill(green)
 
 
 size = 30
-#Creating a circle class
-class ThaCircle:
-    """Class for creating balls that has certain movement speed and are displayed on the screen"""
+
+class PoolBalls:
+    """Class that creates balls that have certain movement speed and are displayed on the screen"""
     
     def __init__(self, x, y, xspeed = 3, yspeed = 3, color = (0,0,0), size = 30, width = 0):
         self.x = x
@@ -40,19 +42,19 @@ class ThaCircle:
         #Displaying ball
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
         
-    def reposition(self):
-        #Creating movement for the ball
+    def integrate(self):
+        #Create movements for the ball
         self.x += self.xspeed
         self.y -= self.yspeed
         
-        if self.y < 0 + self.size//2 or self.y > 555 - self.size//2:
+        if self.y < 0 + self.size//2 or self.y > screen_hight - self.size//2:
             self.yspeed *= -1
-        if self.x < 0 + self.size//2 or self.x > 999 - self.size//2:
+        if self.x < 0 + self.size//2 or self.x > screen_width - self.size//2:
             self.xspeed *= -1
             
         
 class Holes:
-    """Class creating and siplaying "holes""""
+    """Class creates and diplays '8 pool ball holes'"""
     def __init__(self, x, y, color = (0,0,0), size = 30, width = 0):
         
         self.x = x
@@ -66,16 +68,19 @@ class Holes:
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
         
 
-#Creating a circle
-ball = ThaCircle(999//2, 555//2)
+#Create a ball
+ball = PoolBalls(screen_width//2, screen_hight//2)
 
-#Creating holes
+#Create holes
 hole_1 = Holes(5,5)
-hole_2 = Holes(5,550)
-hole_3 = Holes(994,5)
-hole_4 = Holes(994,550)
-hole_5 = Holes(999//2,5)
-hole_6 = Holes(999//2,550)
+hole_2 = Holes(5, screen_hight-5)
+hole_3 = Holes(screen_width - 5,5)
+hole_4 = Holes(screen_width -5 ,screen_hight -5)
+hole_5 = Holes(screen_width//2,5)
+hole_6 = Holes(screen_width//2,screen_hight-5)
+
+#Create a list of the holes
+holes = [hole_1, hole_2, hole_3, hole_4, hole_5, hole_6]
 
 
 
@@ -95,16 +100,13 @@ while True:
     
     screen.fill(green)
     ball.display()
-    ball.reposition()
+    ball.integrate()
     
-    #Displaying holes
-    hole_1.display()
-    hole_2.display()
-    hole_3.display()
-    hole_4.display()
-    hole_5.display()
-    hole_6.display()
+    #Display holes
+    for hole in holes:
+        hole.display()
+        
     pygame.display.update()
     
-    #Defining max FPS
+    #Define max FPS
     clock.tick(60)
