717d3499c3a58172f88f156ee1af6538a97fd0b8
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-09-15 17:06:23 -0700

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,7 @@
 """In this project we make a simple version of the snake game,
 where the games start with a snake that has size of 10 and
-can move around and if hit the walls only, you loose"""
+can move around. Randomly food appears that the snake can "eat",
+were then get larger by 1. If hit the walls or itself, you loose"""
 
 import sys
 import pygame
@@ -49,6 +50,7 @@ class Snake():
         self.positions = [((screen_width / 2), (screen_height / 2))]
         self.direction = random.choice([up, down, left, right])
         self.color = black
+        self.score = 0
 
     def get_head_position(self):
         # Get position of the head of the snake
@@ -84,6 +86,7 @@ class Snake():
         self.lenth = 10
         self.positions = [((screen_width/2), (screen_height/2))]
         self.direction = random.choice([up, down, left, right])
+        self.score = 0
 
     def draw(self, surface):
         for p in self.positions:
@@ -110,6 +113,22 @@ class Snake():
                     sys.exit()
 
 
+class Food():
+    
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = (0, 0, 0)
+        self.random_position()
+
+    def random_position(self):
+        self.position = (random.randint(grid_size, grid_width -2)*grid_size,
+                         random.randint(grid_size, grid_height -2)*grid_size)
+
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (grid_size, grid_size))
+        pygame.draw.rect(surface, self.color, r)
+
+
 def draw_grid(surface):
     for y in range(0, int(grid_height)):
         for x in range(0, int(grid_width)):
@@ -124,6 +143,7 @@ def draw_grid(surface):
 
 
 snake = Snake()
+food = Food()
 
 
 # Game main
@@ -137,13 +157,19 @@ def main():
     surface = surface.convert()
     draw_grid(surface)
 
+
     while True:
 
         clock.tick(10)
         snake.handle_keys()
         draw_grid(surface)
         snake.move()
+        if snake.get_head_position() == food.position:
+            snake.length += 1
+            snake.score += 1
+            food.random_position()
         snake.draw(surface)
+        food.draw(surface)
         screen.blit(surface, (0, 0))
         pygame.display.update()
 
