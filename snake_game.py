b1df91ca49315705597a9e7a235bbe0b3ed18b7e
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-09-16 18:23:42 -0700

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -27,17 +27,17 @@ screen_width = 960
 pygame.display.set_caption("Snake Game")
 
 # Grid
-grid_size = 20
-grid_width = int(screen_width / grid_size)
-grid_height = int(screen_height / grid_size)
+cell_size = 20
+grid_width = int(screen_width / cell_size)
+grid_height = int(screen_height / cell_size)
 
 # Grid Walls
-y_axel = list(range(0, screen_height, grid_size))
-y_walls = (list((0, o) for o in y_axel) +
-           list((screen_width - grid_size, o) for o in y_axel))
-x_axel = list(range(0, screen_width, grid_size))
-x_walls = (list((o, 0) for o in x_axel) +
-           list((o, screen_height - grid_size) for o in x_axel))
+y_axis = list(range(0, screen_height, cell_size))
+y_walls = (list((0, y) for y in y_axis) +
+           list((screen_width - cell_size, y) for y in y_axis))
+x_axis = list(range(0, screen_width, cell_size))
+x_walls = (list((x, 0) for x in x_axis) +
+           list((x, screen_height - cell_size) for x in x_axis))
 walls = x_walls + y_walls
 
 
@@ -46,7 +46,6 @@ class Snake():
     terminates the game if hits wall."""
 
     def __init__(self):
-
         self.length = 10
         self.positions = [((screen_width / 2), (screen_height / 2))]
         self.direction = random.choice([up, down, left, right])
@@ -60,7 +59,7 @@ class Snake():
     def turn(self, point):
         # Turn the snake to all direcations except the oposite
         # of it's current if longer than 1
-        if self.length > 1 and (point[0]*-1, point[1]*-1) == self.direction:
+        if self.length > 1 and (-point[0], -point[1]) == self.direction:
             return
         else:
             self.direction = point
@@ -68,8 +67,8 @@ class Snake():
     def move(self):
         cur = self.get_head_position()
         x, y = self.direction
-        new = (((cur[0] + (x*grid_size)) % screen_width),
-               (cur[1] + (y*grid_size)) % screen_height)
+        new = (((cur[0] + (x*cell_size)) % screen_width),
+               (cur[1] + (y*cell_size)) % screen_height)
 
         # If the new position of head overlaps any
         # of the other positions of the snake, game is ended
@@ -91,15 +90,14 @@ class Snake():
 
     def draw(self, surface):
         for p in self.positions:
-            r = pygame.Rect((p[0], p[1]), (grid_size, grid_size))
+            r = pygame.Rect((p[0], p[1]), (cell_size, cell_size))
             pygame.draw.rect(surface, self.color, r)
             pygame.draw.rect(surface, white, r, 1)
 
     def handle_keys(self):
-
         # Keys Dictionarie
-        keys_d = {pygame.K_UP:up, pygame.K_DOWN:down,
-                  pygame.K_LEFT:left, pygame.K_RIGHT:right}
+        keys_d = {pygame.K_UP: up, pygame.K_DOWN: down,
+                  pygame.K_LEFT: left, pygame.K_RIGHT: right}
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -112,20 +110,27 @@ class Snake():
                     pygame.quit()
                     sys.exit()
 
+    def eat_food(self):
+        if self.get_head_position() == Food.random_position(food):
+            self.length += 1
+            self.score += 1
+            Food.random_position(food)
+
 
 class Food():
-    
+
     def __init__(self):
         self.position = (0, 0)
         self.color = red
         self.random_position()
 
     def random_position(self):
-        self.position = (random.randint(grid_size, grid_width -2)*grid_size,
-                         random.randint(grid_size, grid_height -2)*grid_size)
+        self.position = (random.randint(cell_size, grid_width - 2) * cell_size,
+                         random.randint(cell_size, grid_height - 2) * cell_size)
 
     def draw(self, surface):
-        r = pygame.Rect((self.position[0], self.position[1]), (grid_size, grid_size))
+        r = pygame.Rect((self.position[0], self.position[1]),
+                        (cell_size, cell_size))
         pygame.draw.rect(surface, self.color, r)
 
     def reset(self):
@@ -133,10 +138,9 @@ class Food():
 
 
 def draw_grid(surface):
-
     for y in range(0, int(grid_height)):
         for x in range(0, int(grid_width)):
-            r = pygame.Rect((x*grid_size, y*grid_size),(grid_size, grid_size))
+            r = pygame.Rect((x*cell_size, y*cell_size), (cell_size, cell_size))
             if (x+y) % 2 == 0:
                 pygame.draw.rect(surface, dark_green, r)
             else:
@@ -165,10 +169,7 @@ def main():
         snake.handle_keys()
         draw_grid(surface)
         snake.move()
-        if snake.get_head_position() == food.position:
-            snake.length += 1
-            snake.score += 1
-            food.random_position()
+        snake.eat_food()
         snake.draw(surface)
         food.draw(surface)
         screen.blit(surface, (0, 0))
