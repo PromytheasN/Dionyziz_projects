8edeca5840f13e84359db59351f69e1b74699baf
Alexander Kemos <alforeverandever@gmail.com>
2020-11-16 12:44:42 -0800

    creating brick module and settings module

---
+++ arkanoid/brick.py
@@ -0,0 +1,17 @@
+import pygame
+import settings
+
+class Brick(pygame.sprite.Sprite):
+
+    def __init__(self, point_value, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface(brick_dimentions)
+        self.image.fill(purple)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.point_value = point_value
+
+    def collide(self):
+        self.point_value -= 1
+        if self.point_value == 0:
+            self.kill()
---
+++ arkanoid/settings.py
@@ -0,0 +1,34 @@
+import pygame
+
+# Set colors R G B
+white = (255, 255, 255)
+black = (0, 0, 0)
+orange = (255, 100, 10)
+light_blue = (0, 144, 255)
+shadow = (192, 192, 192)
+purple = (152, 0, 152)
+
+# Display
+display_height = 999
+display_width = 444
+pygame.display.set_caption = ("Arkanoid 1.0")
+FPS = 60
+
+# Movement speed
+speed = display_width // 60
+
+# Movements
+left = (-speed, 0)
+right = (speed, 0)
+up = (0, speed)
+diagonal_left = [-speed, -speed]
+diagonal_right = [speed, -speed]
+
+# Game objects dimentions
+paddle_dimentions = (display_width // 2, display_height // 100)
+brick_width = display_width // 20 * 2
+brick_height = display_height // 100
+brick_dimentions = [brick_width, brick_height] 
+ball_dimentions = (display_height // 100, display_height // 100)
+
+print("settings are ok!")
